587ed7bd27ee524f40becfe86250ce54fd641810
Zero-Wanted <133066031+Zero-Wanted@users.noreply.github.com>
2025-03-31 01:22:44 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-ser = serial.Serial("/dev/serial0",9600,timeout=1)
-
-while True:
-    if ser.in_waiting:
-        line = ser.readline().decode("utf-8").strip()
-        print(f"Received: {line}")
