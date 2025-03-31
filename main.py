28def700bd085942eea0d0aeae4b8bbf3c54bea1
Zero-Wanted <133066031+Zero-Wanted@users.noreply.github.com>
2025-03-31 01:22:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import serial
 
-#Open UART serial port (TX/RX)
 ser = serial.Serial("/dev/serial0",9600,timeout=1)
 
 while True:
-    if ser.in_waiting: # check if data is avaiable
+    if ser.in_waiting:
         line = ser.readline().decode("utf-8").strip()
         print(f"Received: {line}")
